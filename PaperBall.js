53f5303a7de6cffb91663268ada65957d78be614
DevArjunVarma <67416265+DevArjunVarma@users.noreply.github.com>
2020-08-06 14:24:36 +0530

    Add files via upload

---
+++ PaperBall.js
@@ -0,0 +1,27 @@
+class Ball {
+    constructor(x, y, width, height) {
+      var options = {
+          setStatic:false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+      this.body = Bodies.circle(x, y, 10, options);
+      this.radius = 20;
+      this.image=loadImage("sprites/paper.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      fill(225);
+      circle(0, 0, this.radius);
+      pop();
+    }
+  };
+  
